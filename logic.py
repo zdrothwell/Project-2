9745f2c0e79efabe0f4df2ae887150f81f7496cc
zdrothwell <150739940+zdrothwell@users.noreply.github.com>
2023-12-04 16:05:26 -0600

    LOOK AT THIS BRUH

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import *
 from gui import *
-
+import csv
 
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
@@ -10,14 +10,46 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.voteButton.clicked.connect(self.vote_input)
         self.resultsButton.clicked.connect(self.results)
         self.saveButton.clicked.connect(self.save)
+        self.clearButton.clicked.connect(self.clear)
+        self.__cameron_votes = 0
+        self.__allison_votes = 0
+        self.__diego_votes = 0
 
     def vote_input(self):
+        vote_choice = self.dropdownBox.currentIndex()
+        if vote_choice == 1:
+            self.__cameron_votes += 1
+            self.label_2.setText("Voted!")
+        elif vote_choice == 2:
+            self.__allison_votes += 1
+            self.label_2.setText("Voted!")
+        elif vote_choice == 3:
+            self.__diego_votes += 1
+            self.label_2.setText("Voted!")
+        else:
+            self.label_2.setText("You need to select a candidate first.")
+        self.dropdownBox.setCurrentIndex(0)
         return
 
     def results(self):
-        self.label_2.setText("Bruh")
+        self.label_2.setText(f"Votes for Candidate Totals"
+                             f"\nCameron: {self.__cameron_votes}"
+                             f"\nAllison: {self.__allison_votes}"
+                             f"\nDiego: {self.__diego_votes}")
         return
 
     def save(self):
+        with open('vote_totals.csv', 'w', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(['Candidate', 'Votes'])
+            writer.writerow(['Cameron', self.__cameron_votes])
+            writer.writerow(['Allison', self.__allison_votes])
+            writer.writerow(['Diego', self.__diego_votes])
+        self.label_2.setText("Vote totals have been saved to a file")
+        return
+
+    def clear(self):
+        self.label_2.setText("")
+        self.dropdownBox.setCurrentIndex(0)
         return
 
